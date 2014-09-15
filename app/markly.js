9035f34633be153421594dca8869d9bd07bbe2fc
Niraj Pant <nirajpant7@gmail.com>
2014-09-14 19:27:22 -0500

    formatting

--- app/markly.js
+++ app/markly.js
@@ -1,27 +1,25 @@
-angular.module('marklyApp', [
-
-])
+angular.module('marklyApp', [])
 .controller('MainCtrl', function($scope) {
-
+  
     //sample data
     $scope.categories = [
-        {"id": 0, "name": "Development"},
-        {"id": 1, "name": "Design"},
-        {"id": 2, "name": "Exercise"},
-        {"id": 3, "name": "Humor"}
+      {"id": 0, "name": "Development"},
+      {"id": 1, "name": "Design"},
+      {"id": 2, "name": "Exercise"},
+      {"id": 3, "name": "Humor"}
     ];
 
     $scope.bookmarks = [
-          {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
-          {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
-          {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
-          {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
-          {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
-          {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
-          {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
-          {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
-          {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
-      ];
+      {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
+      {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
+      {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
+      {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
+      {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
+      {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
+      {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
+      {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
+      {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+    ];
 
       $scope.currentCategory = null;
 
@@ -31,6 +29,9 @@ angular.module('marklyApp', [
 
       $scope.setCurrentCategory = function setCurrentCategory(category) {
           $scope.currentCategory = category;
+
+          cancelCreating();
+          cancelEditing();
       }
 
       // Creating and Editing States
@@ -52,7 +53,7 @@ angular.module('marklyApp', [
       }
 
       $scope.cancelEditing = function()  {
-          $scope.isEditing = false;
+        $scope.isEditing = false;
       }
 
       $scope.shouldShowCreating = function() {
@@ -60,7 +61,7 @@ angular.module('marklyApp', [
       }
 
       $scope.shouldShowEditing = function() {
-          return $scope.isEditing && !$scope.isCreating;
+        return $scope.isEditing && !$scope.isCreating;
       }
 
 });
