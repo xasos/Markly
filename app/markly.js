719b74d42c0fa73358404713381a409ca92596a0
Niraj Pant <nirajpant7@gmail.com>
2014-10-12 20:55:04 -0500

    Update markly.js

--- app/markly.js
+++ app/markly.js
@@ -5,20 +5,20 @@ angular.module('marklyApp', [])
     $scope.categories = [
       {"id": 0, "name": "Development"},
       {"id": 1, "name": "Design"},
-      {"id": 2, "name": "Exercise"},
-      {"id": 3, "name": "Humor"}
+      {"id": 2, "name": "Hackathons"},
+      {"id": 3, "name": "Talks"}
     ];
 
     $scope.bookmarks = [
       {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
-      {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
-      {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
-      {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
-      {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
-      {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
-      {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
-      {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
-      {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+      {"id": 1, "title": "GitHub", "url": "http://github.com", "category": "Development" },
+      {"id": 2, "title": "Dribbble", "url": "http://dribbble.com/", "category": "Design" },
+      {"id": 3, "title": "Flat UI Colors", "url": "flatuicolors.com", "category": "Design" },
+      {"id": 4, "title": "MHacks", "url": "http://www.mhacks.com/", "category": "Hackathons" },
+      {"id": 5, "title": "Major League Hacking", "url": "http://mlh.io", "category": "Hackathons" },
+      {"id": 6, "title": "Google I/O 2011: HTML5 versus Android: Apps or Web for Mobile Development?", "url": "https://www.youtube.com/watch?v=4f2Zky_YyyQ", "category": "Talks" },
+      {"id": 7, "title": "Chamath Palihapitiya - how we put Facebook on the path to 1 billion users", "url": "https://www.youtube.com/watch?v=raIUQP71SBU", "category": "Talks" },
+      {"id": 8, "title": "Wat", "url": "https://www.destroyallsoftware.com/talks/wat", "category": "Talks" }
     ];
 
       $scope.currentCategory = null;
